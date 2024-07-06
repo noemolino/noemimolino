668984edf47bb9e29d9e39354b69fffa15ab4ca5
Noemi Molino <noemimolino99@gmail.com>
2024-07-06 18:06:10 +0200

    styling

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,42 @@
 // jQuery
-const scriptJQuery = document.createElement('script');
-scriptJQuery.src = 'https://code.jquery.com/jquery-3.5.1.slim.min.js';
-scriptJQuery.integrity = 'sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj';
-scriptJQuery.crossOrigin = 'anonymous';
-document.head.appendChild(scriptJQuery);
+// const scriptJQuery = document.createElement('script');
+// scriptJQuery.src = 'https://code.jquery.com/jquery-3.5.1.slim.min.js';
+// scriptJQuery.integrity = 'sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj';
+// scriptJQuery.crossOrigin = 'anonymous';
+// document.head.appendChild(scriptJQuery);
 
 // Popper.js
-const scriptPopper = document.createElement('script');
-scriptPopper.src = 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js';
-scriptPopper.integrity = 'sha384-ZmpiyWGzwSmHI/7kzPgZdzNgf9pqMgn4kQW5hvpt4ySFP4YJBNO1Wt9CUX5mrpXX';
-scriptPopper.crossOrigin = 'anonymous';
-document.head.appendChild(scriptPopper);
+// const scriptPopper = document.createElement('script');
+// scriptPopper.src = 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js';
+// scriptPopper.integrity = 'sha384-ZmpiyWGzwSmHI/7kzPgZdzNgf9pqMgn4kQW5hvpt4ySFP4YJBNO1Wt9CUX5mrpXX';
+// scriptPopper.crossOrigin = 'anonymous';
+// document.head.appendChild(scriptPopper);
 
-// Bootstrap JS
-const scriptBootstrap = document.createElement('script');
-scriptBootstrap.src = 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js';
-scriptBootstrap.integrity = 'sha384-B4gt1jrGC7Jh4Agp8K1aQ8ohOV7coD2XWZdLM3/PMZam2E1MqqcxEu59kXAm4nxk';
-scriptBootstrap.crossOrigin = 'anonymous';
-document.head.appendChild(scriptBootstrap);
+// // Bootstrap JS
+// const scriptBootstrap = document.createElement('script');
+// scriptBootstrap.src = 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js';
+// scriptBootstrap.integrity = 'sha384-B4gt1jrGC7Jh4Agp8K1aQ8ohOV7coD2XWZdLM3/PMZam2E1MqqcxEu59kXAm4nxk';
+// scriptBootstrap.crossOrigin = 'anonymous';
+// document.head.appendChild(scriptBootstrap);
 
 // JS code
-document.addEventListener('DOMContentLoaded', function() {
-    
+let isMenuOpen = false;
+
+function toggleMenu(x) {
+    x.classList.toggle("change");
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle("active");
+    isMenuOpen = navLinks.classList.contains("active");
+}
+
+// Close the menu when clicking outside
+document.addEventListener('click', function(event) {
+    const isClickInsideHamburger = document.querySelector('.hamburger').contains(event.target);
+    const isClickInsideNavLinks = document.querySelector('.nav-links').contains(event.target);
+
+    if (isMenuOpen && !isClickInsideHamburger && !isClickInsideNavLinks) {
+        document.querySelector('.nav-links').classList.remove("active");
+        document.querySelector('.hamburger').classList.remove("change");
+        isMenuOpen = false;
+    }
 });
